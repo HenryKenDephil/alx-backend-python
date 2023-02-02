354d6bdef3f35cd9769e43f18dacba4a43898a8e
HenryKenDephil <h.komoloh@gmail.com>
2023-02-02 23:20:13 +0300

    edit

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -4,6 +4,7 @@
 def concat(str1: str, str2: str) -> str:
     '''a function that takes two strings arguments
     and returns returns a concatenated string
+    Return: a string
     '''
     return str1 + str2
 
